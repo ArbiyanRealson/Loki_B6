b6229be02614ae7b26caabe912a7495cc4c72056
ArbiyanRealson <realsonarbiyan0@gmail.com>
2022-09-22 21:08:36 +0700

    .

---
+++ app/src/main/java/com/example/testt/Seminar_Hasil.java
@@ -0,0 +1,14 @@
+package com.example.testt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Seminar_Hasil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seminar_hasil);
+    }
+}
