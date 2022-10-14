7799133ae6afc7d1eb8a4601d50d3ce3dab594d9
ArbiyanRealson <realsonarbiyan0@gmail.com>
2022-10-14 20:23:10 +0700

    1234

--- app/src/main/java/com/example/testt/JadwalPeserta.java
+++ app/src/main/java/com/example/testt/JadwalPeserta.java
@@ -2,7 +2,9 @@ package com.example.testt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class JadwalPeserta extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class JadwalPeserta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jadwal_peserta);
+    }
+    public void detailSeminar(View view)
+    {
+        Intent intent = new Intent(JadwalPeserta.this, DetailSeminar.class);
+        startActivity(intent);
+    }
+
+    public void tambahPeserta(View view)
+    {
+        Intent intent = new Intent(JadwalPeserta.this, TambahPeserta.class);
+        startActivity(intent);
     }
 }
