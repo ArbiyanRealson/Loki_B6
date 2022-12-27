3924dbd4fde00726f2a37f8996b7149d5f4d9927
ArbiyanRealson <realsonarbiyan0@gmail.com>
2022-12-27 23:26:29 +0700

    fix

--- app/src/main/java/com/example/testt/retrofit/StoryClient.java
+++ app/src/main/java/com/example/testt/retrofit/StoryClient.java
@@ -1,14 +1,19 @@
 package com.example.testt.retrofit;
 
 import com.example.testt.datamodels.LoginResponse;
+import com.example.testt.datamodels.LogoutResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface StoryClient {
     @FormUrlEncoded
     @POST("/api/login")
     Call<LoginResponse> login(@Field("username") String username, @Field("password") String password);
+
+    @POST("/api/logout")
+    Call<LogoutResponse> logout(@Header("Authorization") String token);
 }
--- app/src/main/java/com/example/testt/semhas.java
+++ app/src/main/java/com/example/testt/semhas.java
@@ -1,20 +1,103 @@
 package com.example.testt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.testt.datamodels.LogoutResponse;
+import com.example.testt.retrofit.StoryClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import com.example.testt.RetrofitClientInstance;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.mlkit.common.sdkinternal.SharedPrefManager;
 
 public class semhas extends AppCompatActivity {
+    String token;
+    Button log_out;
+    SharedPrefManager sharedPrefManager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_semhas);
+
+        SharedPreferences sharedPref = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        token = sharedPref.getString("TOKEN","");
+        sharedPrefManager = new SharedPrefManager(this);
+
+        log_out = findViewById(R.id.log_out);
+
+        log_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                logout();
+            }
+
+
+
+    public void logout(){
+        StoryClient client = RetrofitClientInstance.getRetrofitInstance().create(StoryClient.class);
+
+
+        Call<LogoutResponse> call = client.logout("Bearer " + token );
+        call.enqueue(new Callback<LogoutResponse>() {
+            @Override
+            public void onResponse(Call<LogoutResponse> call, Response<LogoutResponse> response) {
+                if(response.isSuccessful()){
+                    LogoutResponse ganti = response.body();
+                    Toast.makeText(semhas.this, ganti.getMessage(), Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(semhas.this, splash.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LogoutResponse> call, Throwable t) {
+
+            }
+        });
     }
+  });}
 
     public void aju(View view) {
         Intent intent = new Intent(semhas.this,Seminar_Hasil.class);
         startActivity(intent);
     }
+
+    public void sidang(View view) {
+        Intent intent = new Intent(semhas.this, sidang_ta.class);
+        startActivity(intent);
+    }
+
+
+    public void logout(View view) {
+        Intent intent = new Intent(semhas.this, splash.class);
+        startActivity(intent);
+    }
 }
+
+//    public void logout(View view) {
+//        Intent intent = new Intent(semhas.this,splash.class);
+//        startActivity(intent);
+//    }
